c769d0c37211a43c6fd94a70172f7e5827a82ae2
andychen146 <763526790@qq.com>
2019-11-17 01:20:14 +0800

    更新接口

--- src/api/protype.js
+++ src/api/protype.js
@@ -3,7 +3,7 @@ import request from '@/util/request'
 //新增、编辑
 export function updateProtype(params) {
     return request({
-        url: '/api/admin.goods_type/setSpec',
+        url: '/api/admin.goods_type/setType',
         method: 'post',
         data: params
     })
@@ -12,7 +12,7 @@ export function updateProtype(params) {
 //获取列表
 export function getProtypeList(params) {
     return request({
-        url: '/api/hub.goods_spec/getList',
+        url: '/api/hub.goods_type/getList',
         method: 'post',
         data: params
     })
@@ -21,7 +21,7 @@ export function getProtypeList(params) {
 //获取详情
 export function getProtypeInfo(params) {
     return request({
-        url: '/api/hub.goods_spec/getInfo',
+        url: '/api/hub.goods_type/getInfo',
         method: 'post',
         data: params
     })
